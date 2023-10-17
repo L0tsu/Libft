87f5e959d249efe97ce29a01c99f5829d887046c
Julien Moreau <lotus@Juliens-MacBook-Air.local>
2023-10-17 15:33:03 -0400

    v0.01/17-10//15

--- libft_main_test.c
+++ libft_main_test.c
@@ -5,15 +5,6 @@
 #include "libft.h"
 #include "colours.h"
 
-int				ft_isalpha(int c);
-int				ft_isdigit(int c);
-int				ft_isalnum(int c);
-int				ft_isascii(int c);
-unsigned int	ft_strlen(const char *s);
-int				ft_isprint(int c);
-void			*ft_memset(void *b, int c, size_x len);
-void			*ft_bzero(void *s, size_x n);
-
 void 	_isascii()
 {
 	unsigned char is_ascii = 0;
--- src/libft.h
+++ src/libft.h
@@ -1,8 +1,14 @@
+#ifndef LIBFT_H
+
 typedef unsigned long long int size_x;
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isalnum(int c);
-int	ft_isascii(int c);
+
+int				ft_isalpha(int c);
+int				ft_isdigit(int c);
+int				ft_isalnum(int c);
+int				ft_isascii(int c);
 unsigned int	ft_strlen(const char *s);
-int	ft_isprint(int c);
-void	*ft_memset(void *b, int c, size_x len);
+int				ft_isprint(int c);
+void			*ft_memset(void *b, int c, size_x len);
+void			ft_bzero(void *s, size_x n);
+
+#endif
