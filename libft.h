dd8361c4e1ee5a3813759242cc34acc492d2bf31
Julien Moreau <julmorea@c2r6p2.42quebec.com>
2023-10-17 12:05:53 -0400

    v0.01

---
+++ libft.h
@@ -0,0 +1,8 @@
+typedef unsigned long long int size_x;
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int	ft_isalnum(int c);
+int	ft_isascii(int c);
+unsigned int	ft_strlen(const char *s);
+int	ft_isprint(int c);
+void	*ft_memset(void *b, int c, size_x len);
