757fee1b5c41b8caae2851d3e8705af556087bd0
Julien Moreau <julmorea@c2r1p3.42quebec.com>
2023-10-21 13:00:57 -0400

    latest

--- colours.h
+++ colours.h
@@ -1,14 +1,27 @@
-#ifndef COLOURS_H
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   colours.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: julmorea <julmorea@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/21 13:00:11 by julmorea          #+#    #+#             */
+/*   Updated: 2023/10/21 13:00:52 by julmorea         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#define BLACK "\033[30m"
-#define RED "\033[31m"
-#define GREEN "\033[32m" 
-#define YELLOW "\033[33m"
-#define BLUE "\033[34m"
-#define MAGENTA "\033[35m" 
-#define CYAN "\033[36m"
-#define WHITE "\033[37m"
-#define BOLD "\033[1m"
-#define DEFAULT "\033[0m"
+#ifndef COLOURS_H
+# define COLOURS_H
+
+# define BLACK "\033[30m"
+# define RED "\033[31m"
+# define GREEN "\033[32m"
+# define YELLOW "\033[33m"
+# define BLUE "\033[34m"
+# define MAGENTA "\033[35m"
+# define CYAN "\033[36m"
+# define WHITE "\033[37m"
+# define BOLD "\033[1m"
+# define DEFAULT "\033[0m"
 
 #endif
